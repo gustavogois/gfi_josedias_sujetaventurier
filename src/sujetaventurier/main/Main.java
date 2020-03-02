6e70fbe3b071f85a7ab65d701464c1dd0d2e6229
GFI\jose.silva <jose.silva@gfi.pt>
2020-03-02 19:07:11 +0000

    #13: Voir les résultats

--- src/sujetaventurier/main/Main.java
+++ src/sujetaventurier/main/Main.java
@@ -13,6 +13,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Chargement chargement = new Chargement();
 		String nomFichier = new String("chemin2.chm");
+		boolean positionValide = true;
+		Cordonees cordoneesFinales;
 		Cordonees cordoneesInitiales = chargement.chargerCordonnees(nomFichier);
 		
 		Joueur joueur = new Joueur("joueur1", cordoneesInitiales);
@@ -20,13 +22,20 @@ public class Main {
 		
 		Jeu jeu = chargement.chargerJeu(joueur, cordoneesInitiales, chemin);
 		
-		boolean positionValide = true;
-		
 		for(Cordonees c : jeu.getChemin()) {
 			if(!jeu.positionEsDansLeMap(c) || !jeu.positionEstValide(c)) {
 				positionValide = !positionValide;
+				System.out.println(c.toString());
+				System.out.println("Position: invalide");
 				break;
 			}
+		}
+		
+		if(positionValide) {
+			int positionFinale = jeu.getChemin().size() - 1;
+			cordoneesFinales = jeu.getChemin().get(positionFinale);
+			System.out.println("Destination: " + cordoneesFinales.toString());
+			System.out.println("Position: valide");
 		}
 	}
 }
