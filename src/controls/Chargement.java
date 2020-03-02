38418c6dd80a2a3e5276d081bbfe21e711bfa2ec
GFI\jose.silva <jose.silva@gfi.pt>
2020-03-02 14:51:04 +0000

    #10: Charger le joueur (classe Joueur)

--- src/controls/Chargement.java
+++ src/controls/Chargement.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import donnees.ChargeurDonnees;
 import sujetaventurier.modeles.Jeu;
+import sujetaventurier.modeles.Joueur;
 import sujetaventurier.modeles.MapJeu;
 
 public class Chargement {
 	public Chargement() {}
 	
-	public Jeu chargerJeu() throws IOException {
+	public Jeu chargerJeu(Joueur joueur) throws IOException {
 		char[][]donneesMap = ChargeurDonnees.chargeMap();
 		MapJeu mapJeu = new MapJeu(donneesMap);
 		return new Jeu(mapJeu,null,null); // paramètres cordonnesinitiales et chemin à inclure en autre développement.
--- src/sujetaventurier/main/Main.java
+++ src/sujetaventurier/main/Main.java
@@ -3,11 +3,14 @@ package sujetaventurier.main;
 import java.io.IOException;
 
 import controls.Chargement;
+import sujetaventurier.modeles.Jeu;
+import sujetaventurier.modeles.Joueur;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Chargement chargement = new Chargement();
-		chargement.chargerJeu();
+		Joueur joueur = new Joueur("joueur1", null);
+		Jeu jeu = chargement.chargerJeu(joueur);
 	}
 }
